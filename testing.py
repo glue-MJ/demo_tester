af3402595e448d2b8390dc43763a672bc74dec9a
quah ming jun <MJQUAH20.20@ichat.sp.edu.sg>
2021-10-01 00:46:37 +0800

    update

--- testing.py
+++ testing.py
@@ -1,9 +1,15 @@
 from typing import Union
 import math
-import numba
+from numba import jit
+
+def factorial(i: int):
+    summation = 1
+    for num in range(1, i+1):
+        summation *= num
+    return summation
 
 def main():
-    print(math.factorial(10))
+    print(factorial(10))
 
 if __name__ == '__main__':
     main()
